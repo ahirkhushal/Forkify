ea9280a5798115a3126ce930e1712ab41d8f3739
ahirkhushal <ahirkhushal559@gmail.com>
2023-06-27 12:37:06 +0530

    added gersearchresults function

--- src/js/model.js
+++ src/js/model.js
@@ -1,11 +1,13 @@
 import { async } from 'regenerator-runtime';
-import { API_URL } from './config.js';
+import { API_URL, RES_PER_PAGE } from './config.js';
 import { getJSON } from './helpers.js';
 export const state = {
   recipe: {},
   search: {
     query: '',
     results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
   },
 };
 
@@ -44,4 +46,13 @@ export const loadSearchResult = async function (query) {
   } catch (err) {
     throw err;
   }
+};
+
+export const getSearchResultspage = function (page = 1) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+
+  return state.search.results.slice(start, end);
 };
