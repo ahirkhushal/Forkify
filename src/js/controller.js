e870d37df5b18755aa4cbdf44602cd2f7ef3eef1
ahirkhushal <ahirkhushal559@gmail.com>
2023-06-27 12:03:53 +0530

    added control recipe functionality

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,54 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+//loading recipe
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    //guard clouse --> when id is empty then returns nothing
+    if (!id) return;
+
+    //render spinner
+    recipeView.renderspinner();
+
+    //LOADING THE RECIPE
+    await model.loadRecipe(id);
+
+    //rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+    console.error(err.message);
+  }
 };
 
-// https://forkify-api.herokuapp.com/v2
+const init = function () {
+  recipeView.addhandlerRender(controlRecipes);
+};
 
-///////////////////////////////////////
+init();
+
+// const resultsContainer = document.querySelector('.results');
+// const foodId = ` <li class="preview">
+// <a class="preview__link preview__link--active" href="${id}">
+//   <figure class="preview__fig">
+//     <img src="${recipe.image}" alt="Test" />
+//   </figure>
+//   <div class="preview__data">
+//     <h4 class="preview__title">${recipe.title}</h4>
+//     <p class="preview__publisher">${recipe.publisher}</p>
+//     <div class="preview__user-generated">
+//       <svg>
+//         <use href="${icons}#icon-user"></use>
+//       </svg>
+//     </div>
+//   </div>
+// </a>
+// </li>
+// `;
+
+//     resultsContainer.insertAdjacentHTML('afterbegin', foodId);
